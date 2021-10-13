abd30948dad0a77bf3b3cb9faceeb8a95b7542c1
Bénas Thomas <benas.thomas@gmail.com>
2021-10-13 17:57:09 +0200

    [add] comment header interaction

--- interactionHandler.h
+++ interactionHandler.h
@@ -20,7 +20,7 @@ public:
     /**
      * @brief isParticipants
      * @param interaction
-     * @return bool if is participants in an interaction
+     * @return bool if a participants is in an interaction
      */
     bool isParticipant(Interaction interaction, Contact contact);
 
@@ -31,16 +31,57 @@ public:
      */
     std::string briefListOfParticipants(Interaction interaction);
 
+    /**
+     * @brief setInteractions
+     * @param interactions : interacations to be added
+     */
     void setInteractions(std::list<Interaction> interactions);
 
+    /**
+     * @brief getAllInteractions
+     * @return list of all interactions
+     */
     std::list<Interaction> getAllInteractions();
+
+    /**
+     * @brief getAllInteractionsByTitle
+     * @param title
+     * @return All interactions with the same title
+     */
     std::list<Interaction> getAllInteractionsByTitle(std::string& title);
+
+    /**
+     * @brief getAllInteractionsByParticipant
+     * @param participant
+     * @return All interactions with the same participant
+     */
     std::list<Interaction> getAllInteractionsByParticipant(Contact& participant);
+
+    /**
+     * @brief getAllInteractionsByDate
+     * @param time
+     * @return All interactions with the same date
+     */
     std::list<Interaction> getAllInteractionsByDate(CurrentTime& time);
 
 
+    /**
+     * @brief deleteAnInteraction
+     * @param InteractionTodelete
+     */
     void deleteAnInteraction(Interaction InteractionTodelete);
+
+    /**
+     * @brief addAnInteraction
+     * @param InteractionToAdd
+     */
     void addAnInteraction(Interaction InteractionToAdd);
+
+    /**
+     * @brief changeParticipantOfAnInteraction
+     * @param InteractionToUpdate
+     * @param newParticipant
+     */
     void changeParticipantOfAnInteraction(Interaction InteractionToUpdate, Contact newParticipant);
 
 
