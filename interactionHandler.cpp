aa84e4d0526b6c513bb2fa175b4d3fc550ef8bc5
Bénas Thomas <benas.thomas@gmail.com>
2021-10-18 13:52:32 +0200

    [add] getAllInteractionByType + getAllInteractionsByParticipants

--- interactionHandler.cpp
+++ interactionHandler.cpp
@@ -40,8 +40,25 @@ std::list<Interaction> InteractionHandler::getAllInteractions()
     return this->interactions;
 }
 
+
+
 std::list<Interaction> InteractionHandler::getAllInteractionsByType(std::string& type)
 {
+    std::list<Interaction> interactionByType;
+    for(const Interaction& interaction : this->interactions)
+        if(interaction.getType() == type)
+            interactionByType.push_back(interaction);
+    return interactionByType;
+}
+
+std::list<Interaction> InteractionHandler::getAllInteractionsByParticipant(Contact &participant)
+{
+    std::list<Interaction> interactionByParticipant;
+    for(const Interaction& interaction : this->interactions)
+        if(interaction.getParticipant() == participant)
+            interactionByParticipant.push_back(interaction);
+    return interactionByParticipant;
+}
 
 std::list<Interaction> InteractionHandler::getAllInteractionsByDate(std::string &time)
 {
