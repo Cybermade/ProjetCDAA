b89ff5c4ce8c6f9819d80c3cdcafcafaebc5f4df
Bénas Thomas <benas.thomas@gmail.com>
2021-12-13 22:29:19 +0100

    [add] export JSON interaction

--- data/export.cpp
+++ data/export.cpp
@@ -12,37 +12,65 @@ void Export::exportToJson()
     QString path = dir.currentPath() + "/data" + "/export.json";
 
     bdd["contacts"] = serializeContactsForJson();
+    bdd["interactions"] = serializeInteractionsForJson();
 
     writeInJsonFile(bdd, path);
 
 }
 
-
 QJsonObject Export::serializeContactsForJson()
 {
     QJsonObject contacts;
 
-    QSqlQuery queryContact(" SELECT * FROM contact;");
+    QSqlQuery queryContacts("SELECT * FROM contact");
 
-    if(!queryContact.exec())
-        showSQLError(queryContact);
+    if(!queryContacts.exec())
+        showSQLError(queryContacts);
     else {
-        while (queryContact.next()) {
+        while (queryContacts.next()) {
             QJsonObject contact;
 
-            contact["lastname"] = queryContact.value(1).toString();
-            contact["firstname"] = queryContact.value(2).toString();
-            contact["compagny"] = queryContact.value(3).toString();
-            contact["mail"] = queryContact.value(4).toString();
-            contact["phone"] = queryContact.value(5).toString();
-            contact["photo"] = queryContact.value(6).toString();
-            contact["creation"] = queryContact.value(7).toString();
-            contact["modification"] = queryContact.value(8).toString();
+            contact["lastname"] = queryContacts.value(1).toString();
+            contact["firstname"] = queryContacts.value(2).toString();
+            contact["compagny"] = queryContacts.value(3).toString();
+            contact["mail"] = queryContacts.value(4).toString();
+            contact["phone"] = queryContacts.value(5).toString();
+            contact["photo"] = queryContacts.value(6).toString();
+            contact["creation"] = queryContacts.value(7).toString();
+            contact["modification"] = queryContacts.value(8).toString();
 
-            contacts[queryContact.value(0).toString()] = contact;
+            contacts[queryContacts.value(0).toString()] = contact;
         }
    }
    return contacts;
+}
+
+QJsonObject Export::serializeInteractionsForJson()
+{
+    QJsonObject interactions;
+
+    QSqlQuery queryInteractions("SELECT * FROM interaction");
+
+    if(!queryInteractions.exec())
+        showSQLError(queryInteractions);
+    else{
+        while(queryInteractions.next())
+        {
+           QJsonObject interaction;
+
+           interaction["type"] = queryInteractions.value(1).toString();
+           interaction["title"] = queryInteractions.value(2).toString();
+           interaction["date"] = queryInteractions.value(3).toString();
+           interaction["participant"] = queryInteractions.value(4).toString();
+           interaction["note"] = queryInteractions.value(5).toString();
+           interaction["modification date"] = queryInteractions.value(6).toString();
+           interaction["creation date"] = queryInteractions.value(7).toString();
+
+           interactions[queryInteractions.value(0).toString()] = interaction;
+        }
+    }
+
+   return interactions;
 }
 
 void Export::writeInJsonFile(QJsonObject jsonObjectToWrite, QString fileToWrite)
