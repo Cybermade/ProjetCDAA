67c9e2b60def865483bb5f52aaaa5c149de92325
Thomas Bénas <benas.thomas@gmail.com>
2021-12-14 09:25:35 +0100

    [fix] tab for private methods

--- data/export.h
+++ data/export.h
@@ -2,7 +2,6 @@
 #include "storagehandler.h"
 #include <string>
 #include <iostream>
-#include <QCursor>
 #include <QJsonDocument>
 #include <QJsonArray>
 #include <QJsonObject>
@@ -15,30 +14,30 @@ class Export : StorageHandler
 {
 
 private:
- /**
- * @brief serializeContactsJson permet de transposer les contacts de la BDD en objet JSON
- * @return l'objet JSON des contacts
- */
-QJsonObject serializeContactsJson();
+     /**
+     * @brief serializeContactsJson permet de transposer les contacts de la BDD en objet JSON
+     * @return l'objet JSON des contacts
+     */
+    QJsonObject serializeContactsJson();
 
-/**
- * @brief serializeContactsJson permet de transposer les interactions de la BDD en objet JSON
- * @return l'objet JSON des interactions
- */
-QJsonObject serializeInteractionsJson();
+    /**
+     * @brief serializeContactsJson permet de transposer les interactions de la BDD en objet JSON
+     * @return l'objet JSON des interactions
+     */
+    QJsonObject serializeInteractionsJson();
 
-/**
- * @brief serializeContactsJson permet de transposer les todos de la BDD en objet JSON
- * @return l'objet JSON des todos
- */
-QJsonObject serializeToDoJson();
+    /**
+     * @brief serializeContactsJson permet de transposer les todos de la BDD en objet JSON
+     * @return l'objet JSON des todos
+     */
+    QJsonObject serializeToDoJson();
 
-/**
- * @brief writeInJsonFile Permet d'écrire un objet JSON dans un fichier
- * @param JsonObjectToWrite l'objet JSON à écrire
- * @param fileToWrite le fichier dans lequel écrire
- */
-void writeInJsonFile(QJsonObject JsonObjectToWrite, QString fileToWrite);
+    /**
+     * @brief writeInJsonFile Permet d'écrire un objet JSON dans un fichier
+     * @param JsonObjectToWrite l'objet JSON à écrire
+     * @param fileToWrite le fichier dans lequel écrire
+     */
+    void writeInJsonFile(QJsonObject JsonObjectToWrite, QString fileToWrite);
 
 public:
     Export();
